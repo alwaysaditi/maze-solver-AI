dcf26581f6f95ea0a9d7bbb8048fd0d156592572
Aditi Paretkar <68003668+alwaysaditi@users.noreply.github.com>
2025-02-24 15:54:38 +0000

    Delete Assignment 1 directory

--- Assignment 1/stats.py
+++
@@ -1,4 +0,0 @@
-#this is a file to store global lists that hold execution metrics which will later be written to an excel sheet
-bfs_results = []  # A global list to store BFS results
-dfs_results = []
-astar_results = []
